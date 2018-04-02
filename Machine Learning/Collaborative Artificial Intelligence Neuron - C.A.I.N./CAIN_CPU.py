c2caee2eb733597c87f6d2b52d2ec844dbc67689
Shourojit Dutt <34480395+shourojitdutt@users.noreply.github.com>
2018-04-02 11:56:08 +0530

    Fixed Command Processing Unit

--- Machine Learning/Collaborative Artificial Intelligence Neuron - C.A.I.N./CAIN_CPU.py
+++ Machine Learning/Collaborative Artificial Intelligence Neuron - C.A.I.N./CAIN_CPU.py
@@ -14,23 +14,36 @@ class CAIN_CPU:
         # to add or remove paths to check in.
         self.CAIN_PATHS = ["/Users/shourojitdutt/CAIN/CAIN_exec"]
 
-    def execute_command(self):
+    def execute_command(command, CAIN_PATHS):
         """Main command execution function"""
         # Dumping a list of files in CAIN path
-        for path in self.CAIN_PATHS:
-            os.system(str(self.CAIN_PATHS[path]) + " >> "
+        for path in range(len(CAIN_PATHS)):
+            os.system("ls " + str(CAIN_PATHS[path]) + " >> "
                       + "/Users/shourojitdutt/CAIN/checkf.txt")
         check_file = open(
             "/Users/shourojitdutt/CAIN/checkf.txt", "r")
         # Reading file information into temporary buffer
         read_check_file = check_file.readlines()
-        for i in len(read_check_file):
-            if self.command in read_check_file[i]:  # If found in CAIN path
-                buff = read_check_file[i]   # Dumping into another buffer
-                # Execute file from CAINpath
-                os.system("python3.6 " + buff[:-1])
-            else:
-                os.system(self.command)     # Did not find file in CAIN path
         check_file.close()  # Closing file to avoid buffer flushing issues
         # Removing checkfile since it isn't needed anymore
         os.system("rm -f /Users/shourojitdutt/CAIN/checkf.txt")
+        for i in range(len(read_check_file)):
+            # If found in CAIN path
+            if command in read_check_file[i]:
+                buff = read_check_file[i]   # Dumping into another buffer
+                # Execute file from CAINpath
+                print("Command '{}' found in CAIN Path".format(command)
+                      + ", executing")
+                for path in CAIN_PATHS:
+                    try:
+                        os.system("python3.6 " + path + "/" +
+                                  str(buff).rstrip())
+                    except Exception:
+                        pass
+                return 0
+        # If it reached here, it did not find file in CAIN path.
+        print(
+            "Command '{}' not found in CAIN Path".format(command)
+            + ", system executing.")
+        os.system(command)
+        return 1
